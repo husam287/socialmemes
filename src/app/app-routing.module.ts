179bce2df301c131dfefc8e043cf294d1da54a3a
husam sherif <husam287@hotmail.com>
2020-08-19 04:52:17 +0200

    Initialize the project and make navbar
    -Make components
    -make the navbar with it's style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PostsComponent } from '../home/posts/posts.component';
+import { MemesComponent } from '../home/memes/memes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:WelcomeComponent,pathMatch:'full'},
+  {path:'home',redirectTo:'home/posts'},
+  {path:'home',children:[
+    {path:'posts',component:PostsComponent},
+    {path:'memes',component:MemesComponent},
+  ]},
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PostsComponent } from '../home/posts/posts.component';
+import { MemesComponent } from '../home/memes/memes.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    WelcomeComponent,
+    PostsComponent,
+    MemesComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
