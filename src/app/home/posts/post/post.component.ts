c775cb79b6326d673454b189add7e8cfba7145a1
Hossam Sherif <husam287@hotmail.com>
2020-09-01 14:11:37 +0200

    add comments in post component

--- src/app/home/posts/post/post.component.ts
+++ src/app/home/posts/post/post.component.ts
@@ -10,17 +10,21 @@ export class PostComponent implements OnInit {
 
   domain=domainName;
   @Input('postData') postData;
-  @Input('indexOfPost') indexOfPost;
-  commentsIsShow=false;
+  @Input('indexOfPost') indexOfPost:number; //index of the post
+
+  commentsIsShow=false; //if comments are shown or not
   comments=[1,2]; //post comments
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  //##### Show-hide comment function #####
   showComments(){
     this.commentsIsShow=!this.commentsIsShow
     if(this.commentsIsShow){
+      //set time to wait untill comment appears to select it successfully
+      //select the element by nth child to not select all classes when repeated by ngfor
       setTimeout(() => {
         document.getElementsByClassName('comments')[this.indexOfPost].scrollIntoView({block:'center',behavior:'smooth'})
       }, 20);
